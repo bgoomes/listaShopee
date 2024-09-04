0645644a42cb6152562d85d2c224fee26813773b
Bruno Gomes <obrunogomes@obrunogomes.local>
2024-09-04 13:32:14 -0300

    update item

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import { Link } from "./link";
 import { links } from "./data/links.json"
+import logo from "./assets/img-logo.jpeg"
  
 export function App() {
  
   return (
-    <div className="w-full h-svh flex justify-center bg-red-700 text-red-50">
+    <div className="w-full min-h-svh flex justify-center bg-red-700 text-red-50">
       <div className="w-96 p-10 flex flex-col gap-10">
         <div className="flex items-center flex-col gap-5">
-          <div className="flex flex-col border-4 items-center w-32 h-32 rounded-full">
-            <img src="#" alt="" />
+          <div className="flex flex-col border-4 items-center w-32 h-32 rounded-full ">
+            <img src={logo} alt="" className="rounded-full"/>
           </div>
           <div className="flex flex-col items-center">
-            <p className="text-2xl">hdashduhasd</p>
-            <span className="text-sm text-red-200">@blabla</span>
+            <p className="text-xl text-center">Lista dos itens divulgados no nosso perfil do Tik Tok</p>
+            <span className="text-sm text-red-200"><a href="https://www.tiktok.com/@favsshopee">@favsshopee</a></span>
           </div>
         </div>
         <div className="flex flex-col gap-4">
             {links.map((item) => {
               return (
-                <Link key={item.id} item={item.name} link={item.link}/>
+                <Link key={item.id} item={item.name} link={item.link} destaque={item.destaque}/>
               )
             })}
         </div>
--- src/link.tsx
+++ src/link.tsx
@@ -3,12 +3,26 @@ import { ArrowUpRight } from "lucide-react"
 interface linkProps {
     link: string
     item: string
+    destaque?: boolean
 }
 
-export function Link({ link, item}: linkProps){
+export function Link({ link, item, destaque}: linkProps){
+    if(destaque){
+        return (
+            <a href={link}>
+            <div className="flex items-center justify-between rounded-xl bg-red-200 shadow-sm border-4 border-red-400 text-red-900 p-4 hover:bg-red-500 hover:text-white cursor-pointer transition-all animate-pulse">
+                <p>{item}</p>
+                <div>
+                   <ArrowUpRight />
+                </div>
+            </div>
+        </a>
+        )
+    }
+
     return (
         <a href={link}>
-            <div className="flex items-center justify-between rounded-xl bg-red-200 shadow-sm text-red-800 p-4 hover:bg-red-300 cursor-pointer transition-all">
+            <div className="flex items-center justify-between rounded-xl bg-red-200 shadow-sm text-red-800 p-4 hover:bg-red-300 hover:text-white cursor-pointer transition-all">
                 <p>{item}</p>
                 <div>
                    <ArrowUpRight />
